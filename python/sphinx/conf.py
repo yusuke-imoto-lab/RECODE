137660ef0edad78a60e10b3a8d7c3a93dff2fccf
yusukeimoto <85976418+yusukeimoto@users.noreply.github.com>
2022-08-16 15:13:55 +0900

    Update conf.py

--- python/sphinx/conf.py
+++ python/sphinx/conf.py
@@ -23,7 +23,7 @@ copyright = '2021, Yusuke Imoto'
 author = 'Yusuke Imoto'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
